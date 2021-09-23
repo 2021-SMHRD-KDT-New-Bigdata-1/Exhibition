c68a74e8587105d605694aa72b54cc8438c96e9e
smhrd <smhrd@222.102.43.31>
2021-09-23 09:38:51 +0900

    확인

--- src/main/java/Test/test01.java
+++ src/main/java/Test/test01.java
@@ -4,6 +4,9 @@ public class test01 {
 
 	public static void main(String[] args) {
 		System.out.println("여러분 제 말이 들리시나요 ... ... ...");
+		
+		
 	}
 
 }
+	
--- src/main/java/Test/test4.java
+++ src/main/java/Test/test4.java
@@ -4,6 +4,7 @@ public class test4 {
 
 	public static void main(String[] args) {
 		System.out.println("0923");
+		System.out.println("0000");
 	}
 
 }
